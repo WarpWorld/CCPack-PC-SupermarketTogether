d53b32a2228e67e91aaa5abe4a1cad18e0ed1774
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2025-09-16 20:43:02 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- SupermarketTogether.cs
+++ SupermarketTogether.cs
@@ -1,4 +1,5 @@
-﻿using ConnectorLib.SimpleTCP;
+﻿using System.Diagnostics.CodeAnalysis;
+using ConnectorLib.SimpleTCP;
 using CrowdControl.Common;
 using ConnectorType = CrowdControl.Common.ConnectorType;
 
@@ -10,6 +11,7 @@ public class SupermarketTogether : SimpleTCPPack<SimpleTCPServerConnector>
 
     public override ushort Port => 51337;
 
+    [SuppressMessage("PackMetadata", "CC1007:Message Format Property")] //todo - check if this should be changed
     public override ISimpleTCPPack.MessageFormatType MessageFormat => ISimpleTCPPack.MessageFormatType.CrowdControlLegacy;
 
     public SupermarketTogether(UserRecord player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
